c5bb3b22a63c1d76e86ba244182248c3b39bed66
gwynfreeman <gwyneth.freeman@wsu.edu>
2026-04-13 09:50:44 -0700

    create san diego button

--- follow-your-heart/scripts.js
+++ follow-your-heart/scripts.js
@@ -1,1 +1,10 @@
+// set up DOM loading event listener 
+document.addEventListener("DOMContentLoaded", function () {
 
+    // find the dark mode button
+    let sanDiegoButton = document.querySelector("#toggleSanDiego");
+
+
+    });
+
+});

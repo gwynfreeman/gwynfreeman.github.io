09085de5f32740fdc7544426fc92c5b624478b95
gwynfreeman <gwyneth.freeman@wsu.edu>
2026-04-14 20:48:16 -0700

    delete js

--- follow-your-heart/scripts.js
+++ follow-your-heart/scripts.js
@@ -1,10 +1,1 @@
-// set up DOM loading event listener 
-document.addEventListener("DOMContentLoaded", function () {
-
-    // find the dark mode button
-    let sanDiegoButton = document.querySelector("#toggleSanDiego");
-
-
-    });
-
 
